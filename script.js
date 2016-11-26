29e4b87811cfa06c534ce701a322f3a30676f88e
Zetagon <leo.ericson@yahoo.se>
2016-11-26 18:25:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function func(n)
 //		document.getElementById("bild").style.left = a + i + 'px';
 //		document.getElementById("bild").innerHTML = a + i + 'px';
 		i++;
-		$("p").innerHTML = "\u0074"
+		document.getElementById("bild").innerHTML = "\u0074"
 	}
 }
 
