eaaff7b4a4bf05d57be308cd9d7b32c4ad2c6c8d
Zetagon <leo.ericson@yahoo.se>
2016-11-27 16:24:10 +0100

    Sync

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ function func(n)
 		document.getElementById("bild").innerHTML = "f\u00F6\u00E4rra(--- something)"
 	}
 }
-
-// var wordFiles = {
-	String.prototype.toUnicode = function(){
-   	 	var result = "";
-    for(var i = 0; i < this.length; i++){
-        result += "\\u" + ("000" + this[i].charCodeAt(0).toString(16)).substr(-4);
-    }		
-    	return result;
-	};
-
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+ var wordFiles = {
+	 
+	var fileone;
+	var filetwo;
+	var input;
+	var answer;
+	var integer;
 	function encodeHTML(str){
  		var aStr = str.split(''),
      	i = aStr.length,
@@ -39,23 +39,70 @@ function func(n)
   		}
 		return aRet.reverse().join('');
 	}
+	function openWordFiles(filename){
+		$.get(filename + "-1.txt", function(data, status)
+		{
+			// alert("Data: " + data + "\nStatus: " + status);
+			 //document.getElementById("bild").innerHTML = "Data: " + data + "\nStatus: " + status;
+			fileone = data.split(/\r\n|\r|\n/g)
+			
+			alert(data);
+		},"text")
+		$.get(filename + "-2.txt", function(data, status)
+		{
+			// alert("Data: " + data + "\nStatus: " + status);
+			 //document.getElementById("bild").innerHTML = "Data: " + data + "\nStatus: " + status;
+			filetwo = data.split(/\r\n|\r|\n/g)
+			
+			alert(data);
+		},"text")
+	}
+	
+	function SaveInput(){
+		return $("#input").html();
+	}
+	function newWord(){
+		 integer = getRandomArbitrary(0, fileone.length);
+		 $("#output").html() = fileone[integer];
+		 answer = filetwo[integer];
+	}
+	function handleInput(){
+		input = saveInput();
+		if(input == answer){
+			$("#output").html() = "correct!";
+			var index = filetwo.indexOf(answer);
+			fileone.splice(index, 1);
+			filetwo.splice(index, 1);
+			
+		}
+		else{
+			$("#output").html() = "Incorrect! The correct answer was: " + answer;
+		}
+		newWord();
+	}
 
-	var fileone;
-	var filetwo;
-	// function getFile(name)
-	// {
-		$(document).ready(function(){
-			$("p").click(function()
-			{
-				$.get("English_must_words-1.txt", function(data, status)
-				{
-					// alert("Data: " + data + "\nStatus: " + status);
-					 //document.getElementById("bild").innerHTML = "Data: " + data + "\nStatus: " + status;
-					alert(data);
-				},"text")
-			})
-		})
-	// }
-// }
+ }
 
 
+
+$(document).ready(function(){
+	$("#restart").click(function{
+		wordFiles.newWord();
+		$("#restart").html() = "start";
+		wordFiles.openWordFiles("English_must_words");
+	})
+		
+	$("p").click(function()
+	{
+		wordFiles.openWordFiles("English_must_words");
+	})
+	
+	//add event handling, check if user presses \n save input
+	document.addEventListener('keydown', function(event){
+		switch(event.keyCode){
+			case 13:
+				wordFiles.handleInput)();
+			break;
+		}
+	})
+})
