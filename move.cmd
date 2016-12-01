f283427350a50712caf43e1e8371d8b72331f05d
Zetagon <leo.ericson@yahoo.se>
2016-12-01 20:50:27 +0100

    Add move.cmd and moveBack.cmd for testing locally with xampp

---
+++ move.cmd
@@ -0,0 +1,3 @@
+move glossary.html "Zetagon.github.io\"
+move script-glossary.js "Zetagon.github.io\"
+move jquery-3.1.1.min.js "Zetagon.github.io\"
---
+++ moveBack.cmd
@@ -0,0 +1,3 @@
+move "Zetagon.github.io\glossary.html" "../Zetagon.github.io"
+move "Zetagon.github.io\script-glossary.js" "../Zetagon.github.io"
+move "Zetagon.github.io\jquery-3.1.1.min.js" "../Zetagon.github.io"
