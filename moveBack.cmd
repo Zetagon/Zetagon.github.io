7847241144dad471a1b1d9c4a04e5cd808263bfb
Draklive <light.xmail@gmail.com>
2016-12-03 19:50:30 +0100

    Delete moveBack.cmd

--- moveBack.cmd
+++
@@ -1,3 +0,0 @@
-move "Zetagon.github.io\glossary.html" "../Zetagon.github.io"
-move "Zetagon.github.io\script-glossary.js" "../Zetagon.github.io"
-move "Zetagon.github.io\jquery-3.1.1.min.js" "../Zetagon.github.io"
