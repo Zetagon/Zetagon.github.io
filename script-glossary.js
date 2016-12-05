f4dd003dc0a3fbf7a0d24a88f5da77679d97ed34
Draklive <light.xmail@gmail.com>
2016-12-05 16:38:04 +0100

    added english plus title

--- script-glossary.js
+++ script-glossary.js
@@ -13,6 +13,7 @@ var firstRound = true;
 
 window.onload = function LoadMenu()
 {
+	document.title = "English Plus";
 	var ListIndex = GetWordListFromServer("Word_List_Index.txt");
 	for(i = 0; i < ListIndex[0].length; i++)
 	{
