c288bbf7e6361a88f0537d02f0bf05b2dd12f3fb
Zetagon <leo.ericson@yahoo.se>
2016-12-09 21:22:05 +0100

    Compiled code, fixed bug, needed for previous commit

--- compile.cmd
+++ compile.cmd
@@ -7,6 +7,6 @@ SET "srcpath=%srcpath%src/glossary/functions/HandleInput.js "
 SET "srcpath=%srcpath%src/glossary/functions/LoadMenu.js "
 SET "srcpath=%srcpath%src/glossary/functions/NewWord.js "
 SET "srcpath=%srcpath%src/glossary/functions/ReverseButtonPressed.js "
-SET "srcpath=%srcpath%src/glossary/functions/StartGlossary.js "
+SET "srcpath=%srcpath%src/glossary/functions/Start_Glossary.js "
 
 java -jar closure-compiler/compiler.jar --js %srcpath% --js_output_file bin/script-glossary-compiled.js
