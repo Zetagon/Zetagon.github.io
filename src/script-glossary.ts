338707f30f11a423018bc567323e0a66707bd02b
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2017-04-13 21:16:00 +0200

    Change GetWordListFromServer to use JSON and rawText

--- src/script-glossary.ts
+++ src/script-glossary.ts
@@ -13,6 +13,7 @@ var correct_words:Array<string> = [];
 var playing:boolean = false;
 var firstRound:boolean = true;
 let sheetAry:Array<Array<string>>
+let questionHandler:QuestionHandler;
 
 
 
@@ -120,6 +121,7 @@ document.getElementById("leosinput").addEventListener("keydown", function(event)
         break;
     }
 });
+
 interface Window {XMLHttpRequest : any; }
 function GetWordListFromServer(filename:string) {
     var ListLeft:Array<string> = [];
@@ -136,16 +138,26 @@ function GetWordListFromServer(filename:string) {
     server_file_request.onreadystatechange = function(){  // This function will run whenever there the state of the XMLHttpRequest updates
 
         if (server_file_request.readyState==4 && server_file_request.status==200){  // When this is the case the response is ready to collect
-
+//========================================================================================================================
             var wordfiletext = server_file_request.responseText;  // The file is saved to a variable in order to not rely on the XMLHttpRequest anymore
-            var wordpairs:Array<string> = wordfiletext.split(/\r\n|\r|\n/g);  // Splitting the text by newlines. Many different versions of newline are used to make sure all browsers understand
-            for (let i = 0; i < wordpairs.length; i++){  // Splitting the wordpairs into induvidual words and saving them to a left and right word-list
-
-                var wordpair = wordpairs[i].split("=");
-                ListLeft[i]=wordpair[0];
-                ListRight[i]=wordpair[1];
+            let isInJsonFormat = true
+            try{// determine if text is in jsonformat
+                var json = JSON.parse(wordfiletext);
+                if(!json.AnswerDescriptionPairs){ // check if json is in the correct format
+                    isInJsonFormat = false;
+                }
             }
-            BothLists = [ListLeft, ListRight];
+            catch(e){
+                isInJsonFormat = false;
+            }
+            if(isInJsonFormat){
+                var json = JSON.parse(wordfiletext);
+                questionHandler = new QuestionHandler(json.AnswerDescriptionPairs);
+            }else{
+                var wordpairs:Array<string> = wordfiletext.split(/\r\n|\r|\n/g);  // Splitting the text by newlines. Many different versions of newline are used to make sure all browsers understand
+                questionHandler =  createQuestionHandlerFromRawText(wordpairs);
+            }
+//========================================================================================================================
         }
     }
 
@@ -153,6 +165,7 @@ function GetWordListFromServer(filename:string) {
     server_file_request.send();  // Sending the request
     return BothLists;
 }
+
 function getRandomArbitrary(min:number, max:number) {
     return Math.floor(Math.random() * (max - min) + min);  // värdet kan aldrig anta MAX då Math.random aldrig kan bli ett därför adderars 1
 }
