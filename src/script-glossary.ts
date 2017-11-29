bbd446fabe4ab06a0d8a13f6400eaa15eecce1c3
Draklive <light.xmail@gmail.com>
2017-11-29 07:21:17 +0100

    No empty lines please

--- src/script-glossary.ts
+++ src/script-glossary.ts
@@ -159,11 +159,14 @@ server_file_request.onreadystatechange = function()  // This function will run w
 	{
 		var wordfiletext = server_file_request.responseText;  // The file is saved to a variable in order to not rely on the XMLHttpRequest anymore
 		var wordpairs:Array<string> = wordfiletext.split(/\r\n|\r|\n/g);  // Splitting the text by newlines. Many different versions of newline are used to make sure all browsers understand
+		wordpairs = wordpairs.filter(function(str) {
+		    return /\S/.test(str);
+		});
 		for (let i = 0; i < wordpairs.length; i++)  // Splitting the wordpairs into induvidual words and saving them to a left and right word-list
 		{
 			var wordpair = wordpairs[i].split("=");
-			ListLeft[i]=wordpair[0];
-			ListRight[i]=wordpair[1];
+			ListLeft[i]=wordpair[0].trim();
+			ListRight[i]=wordpair[1].trim();
 		}
 		BothLists = [ListLeft, ListRight];
     }
